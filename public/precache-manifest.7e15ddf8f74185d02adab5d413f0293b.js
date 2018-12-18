6cc6e52a9033ad7d29627d74d419f651a13519ba
Rashid H Armand <rashidharmand@gmail.com>
2018-12-17 19:24:50 -0500

    production build ready

---
+++ public/precache-manifest.7e15ddf8f74185d02adab5d413f0293b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "198533dcc53817a02b50",
+    "url": "/static/css/main.6192407a.chunk.css"
+  },
+  {
+    "revision": "198533dcc53817a02b50",
+    "url": "/static/js/main.198533dc.chunk.js"
+  },
+  {
+    "revision": "9ba6304a41f418c23573",
+    "url": "/static/js/1.9ba6304a.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ab10d000cdf7ee45a57395feee160ad2",
+    "url": "/static/media/logo.ab10d000.png"
+  },
+  {
+    "revision": "2da9561b4cd8247ebcba56cc1983eaa6",
+    "url": "/index.html"
+  }
+];
--- server.js
+++ server.js
@@ -3,10 +3,21 @@ const graphqlHTTP = require('express-graphql');
 const app = express();
 const PORT = process.env.PORT || 5000;
 const schema = require('./schema');
+const cors = require('cors');
+const path = require('path');
+
+// Allow cross-origin
+app.use(cors());
 
 app.use('/graphql', graphqlHTTP({
   schema,
   graphiql: true
 }));
 
-app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
+app.use(express.static('public'));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+});
+
+app.listen(PORT, () => console.log(`Server started on port: ${ PORT }`));
